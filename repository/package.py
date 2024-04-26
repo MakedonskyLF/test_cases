9f4669d4b39f286ec69c0ba5546cf2cf3a1c06d6
MakedonskyLF <Polyakov.main@gmail.com>
2024-04-26 22:47:17 +0300

    Package added getid and isnewer.

--- repository/package.py
+++ repository/package.py
@@ -1,9 +1,12 @@
+from collections.abc import Hashable
 from dataclasses import dataclass
+
+from rpm_vercmp import vercmp
 
 
 @dataclass
 class Package:
-    """class for representing one package"""
+    """class for representing package"""
 
     name: str
     epoch: int
@@ -13,3 +16,24 @@ class Package:
     disttag: str
     buildtime: int
     source: str
+
+    def getid(self) -> Hashable:
+        """Returns unique package identifier"""
+        return (self.name, self.arch)
+
+    def isnewer(self, other_package: "Package") -> bool:
+        """Compare package version with version of other package according RPM Package Manager version comparison algorithm
+
+        Args:
+            other_package (Package): Other package to compare
+
+        Returns:
+            bool: is self newer than other package
+        """
+        if self.getid() != other_package.getid():
+            raise ValueError("Trying to compare version of different packages.")
+
+        if self.version == other_package.version:
+            return vercmp(self.release, other_package.release) == 1
+        else:
+            return vercmp(self.version, other_package.version) == 1
